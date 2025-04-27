83044fb2ed403e602bc10618fbbdaa1eec350230
ItsLuis19 <xluisalbert15@gmail.com>
2025-04-26 19:31:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
-// Incluir configuración
 require_once 'config/config.php';
 
-// Verificar si hay sesión iniciada
 if (!isAuthenticated()) {
-    // Redirigir al login
     redirect('/views/auth/login.php');
 }
 
-// Si hay sesión, redirigir al dashboard
 redirect('/views/dashboard/index.php');
 ?> 
